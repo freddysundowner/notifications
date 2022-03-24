2fb28290a416b58de99a7b3c70b4206a18ad288b
Grace Njuguna <gracemakainjuguna@gmail.com>
2022-03-24 09:49:37 +0300

    individual order screen

--- lib/screens/profile/orders_sceen.dart
+++ lib/screens/profile/orders_sceen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fluttergistshop/controllers/user_controller.dart';
 import 'package:fluttergistshop/models/orders_model.dart';
+import 'package:fluttergistshop/screens/profile/individual_order.dart';
 import 'package:fluttergistshop/services/end_points.dart';
 import 'package:fluttergistshop/utils/functions.dart';
 import 'package:get/get.dart';
@@ -31,101 +32,106 @@ class OrdersScreen extends StatelessWidget {
                     itemBuilder: (context, index) {
                       OrdersModel ordersModel = OrdersModel.fromJson(
                           _userController.userOrders.elementAt(index));
-                      return Padding(
-                        padding: const EdgeInsets.only(
-                            left: 10.0, right: 10, bottom: 20),
-                        child: Column(
-                          children: [
-                            Align(
-                                alignment: Alignment.centerRight,
-                                child: Text(
-                                  convertTime(ordersModel.date.toString()),
-                                  style: TextStyle(
-                                      color: Colors.black, fontSize: 12.sp),
-                                )),
-                            SizedBox(height: 0.01.sh,),
-                            Container(
-                              padding: const EdgeInsets.all(20),
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: Row(
-                                children: [
-                                  Container(
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(5),
-                                        color: Colors.white),
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Center(
-                                        child:Image.network(
-                                          imageUrl + ordersModel.itemId!.productId!.images!.first,
-                                          height: 0.1.sh,
-                                          width: 0.2.sw,
-                                          fit: BoxFit.fill,
+                      return InkWell(
+                        onTap: () {
+                          Get.to(IndividualOrderScreen(ordersModel));
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                              left: 10.0, right: 10, bottom: 20),
+                          child: Column(
+                            children: [
+                              Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Text(
+                                    convertTime(ordersModel.date.toString()),
+                                    style: TextStyle(
+                                        color: Colors.black, fontSize: 12.sp),
+                                  )),
+                              SizedBox(height: 0.01.sh,),
+                              Container(
+                                padding: const EdgeInsets.all(20),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(5),
+                                          color: Colors.white),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Center(
+                                          child:Image.network(
+                                            imageUrl + ordersModel.itemId!.productId!.images!.first,
+                                            height: 0.1.sh,
+                                            width: 0.2.sw,
+                                            fit: BoxFit.fill,
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                  SizedBox(width: 0.02.sw,),
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Align(
-                                          alignment: Alignment.centerLeft,
-                                          child: Text(
-                                            ordersModel.itemId!.productId!.name!,
-                                            style: TextStyle(
-                                                color: Colors.black, fontSize: 16.sp),
-                                          )),
-                                      Row(
-                                        children: [
-                                          Text(
-                                            "Status: ",
-                                            style: TextStyle(
-                                                color: Colors.black, fontSize: 12.sp),
-                                          ),
-                                          Text(
-                                            ordersModel.status!,
-                                            style: TextStyle(
-                                                color: Colors.black, fontSize: 12.sp),
-                                          ),
-                                        ],
-                                      ),
-                                      Row(
-                                        children: [
-                                          Text(
-                                            "Quantity: ",
-                                            style: TextStyle(
-                                                color: Colors.black, fontSize: 12.sp),
-                                          ),
-                                          Text(
-                                            ordersModel.itemId!.quantity.toString(),
-                                            style: TextStyle(
-                                                color: Colors.black, fontSize: 12.sp),
-                                          ),
-                                        ],
-                                      ),
-                                      Row(
-                                        children: [
-                                          Text(
-                                            "Total: ",
-                                            style: TextStyle(
-                                                color: Colors.black, fontSize: 12.sp),
-                                          ),
-                                          Text(
-                                            ordersModel.totalCost.toString(),
-                                            style: TextStyle(
-                                                color: Colors.black, fontSize: 12.sp),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                ],
+                                    SizedBox(width: 0.02.sw,),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Align(
+                                            alignment: Alignment.centerLeft,
+                                            child: Text(
+                                              ordersModel.itemId!.productId!.name!,
+                                              style: TextStyle(
+                                                  color: Colors.black, fontSize: 16.sp),
+                                            )),
+                                        Row(
+                                          children: [
+                                            Text(
+                                              "Status: ",
+                                              style: TextStyle(
+                                                  color: Colors.black, fontSize: 12.sp),
+                                            ),
+                                            Text(
+                                              ordersModel.status!,
+                                              style: TextStyle(
+                                                  color: Colors.black, fontSize: 12.sp),
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text(
+                                              "Quantity: ",
+                                              style: TextStyle(
+                                                  color: Colors.black, fontSize: 12.sp),
+                                            ),
+                                            Text(
+                                              ordersModel.itemId!.quantity.toString(),
+                                              style: TextStyle(
+                                                  color: Colors.black, fontSize: 12.sp),
+                                            ),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text(
+                                              "Total: ",
+                                              style: TextStyle(
+                                                  color: Colors.black, fontSize: 12.sp),
+                                            ),
+                                            Text(
+                                              ordersModel.totalCost.toString(),
+                                              style: TextStyle(
+                                                  color: Colors.black, fontSize: 12.sp),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       );
                     })
